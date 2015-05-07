028d5d6e886a48dacc7f7d5a4126aabd4df4f3ed
TJ Holowaychuk <tj@vision-media.ca>
2015-05-07 14:54:13 -0700

    add missing comment

--- server.go
+++ server.go
@@ -35,6 +35,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Publish requests to NSQD.
 func (s *Server) publish(w http.ResponseWriter, r *http.Request) {
 	var body map[string]interface{}
 
