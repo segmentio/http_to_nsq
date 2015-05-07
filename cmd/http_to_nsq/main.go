bd26659655cbdf21d5a6240308101b65c096cbc2
TJ Holowaychuk <tj@vision-media.ca>
2015-05-07 13:01:13 -0700

    add boot logging

--- cmd/http_to_nsq/main.go
+++ cmd/http_to_nsq/main.go
@@ -11,11 +11,12 @@ var Version = "0.0.1"
 
 const Usage = `
   Usage:
-    http_to_nsq [--nsqd-tcp-address addr] [--address addr]
+    http_to_nsq --topic name [--nsqd-tcp-address addr] [--address addr]
     http_to_nsq -h | --help
     http_to_nsq --version
 
   Options:
+    --topic name             nsqd topic name
     --address addr           bind address [default: localhost:3000]
     --nsqd-tcp-address addr  nsqd tcp address [default: localhost:4150]
     -h, --help               output help information
@@ -29,19 +30,24 @@ func main() {
 		log.Fatal("error: %s", err)
 	}
 
+	topic := args["--topic"].(string)
+	addr := args["--address"].(string)
 	nsqd := args["--nsqd-tcp-address"].(string)
+
+	log.Printf("starting http_to_nsq %s", Version)
+	log.Printf("publishing to %s as topic %q", nsqd, topic)
+
 	prod, err := nsq.NewProducer(nsqd, nsq.NewConfig())
 	if err != nil {
 		log.Fatal("error starting producer: %s", err)
 	}
 
-	server := http_to_nsq.Server{
+	server := &http_to_nsq.Server{
 		Log:       log.New(os.Stderr, "http_to_nsq", log.LstdFlags),
 		Topic:     "builds",
 		Publisher: prod,
 	}
 
-	addr := args["--address"].(string)
 	err = http.ListenAndServe(addr, server)
 	if err != nil {
 		log.Fatal("error binding: %s", err)
